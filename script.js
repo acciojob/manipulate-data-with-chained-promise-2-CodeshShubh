731d10fb8ef3e9639fb27d473ea04990db5762fd
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-02-01 18:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+let arr = [1,2,3,4];
+let output = document.getElementById('output');
+function displays(){
+	return new Promise ((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000)
+	})
+}
+
+displays().then((arr)=>{
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			let filterArr(()=>{
+				let filterArr = arr.filter((item)=>item%2==0);
+				resolve(filterArr);
+			},1000)
+		})
+	})
+}).then((arr)=>{
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			let double = arr.map((item)=>item*2);
+		},2000)
+	})
+}).then((doubleArr)=>{
+	output.textContent = `Even Numbers: ${doubleArr}`
+})
+.catch((error) => { console.log('error', error); })
